991148e34c5dde48a458596a5a5db0c314259175
Andrew Fitz Gibbon <fitz@temporal.io>
2023-04-17 11:47:08 -0700

    add more 'email customer' activities for different lifecycle events

--- activities.go
+++ activities.go
@@ -9,15 +9,27 @@ import (
 
 type Activities struct{}
 
-func (a *Activities) SendWelcomeEmail(ctx context.Context, customer CustomerInfo) error {
+func (a *Activities) SendWelcomeEmail(ctx context.Context, subscription SubscriptionInfo) error {
 	logger := activity.GetLogger(ctx)
-	logger.Info("Sending welcome email to " + customer.Email)
+	logger.Info("Sending welcome email to " + subscription.Customer.Email)
 	return nil
 }
 
-func (a *Activities) ChargeSubscription(ctx context.Context, customer CustomerInfo, subscription SubscriptionInfo) error {
+func (a *Activities) SendTrialExpiredEmail(ctx context.Context, subscription SubscriptionInfo) error {
 	logger := activity.GetLogger(ctx)
-	logger.Info("Charging " + customer.Email + " the " +
+	logger.Info("Sending trial is over email to " + subscription.Customer.Email)
+	return nil
+}
+
+func (a *Activities) SendCancelationEmail(ctx context.Context, subscription SubscriptionInfo) error {
+	logger := activity.GetLogger(ctx)
+	logger.Info("Sending 'sorry to see you go' email to " + subscription.Customer.Email)
+	return nil
+}
+
+func (a *Activities) ChargeSubscription(ctx context.Context, subscription SubscriptionInfo) error {
+	logger := activity.GetLogger(ctx)
+	logger.Info("Charging " + subscription.Customer.Email + " the " +
 		fmt.Sprintf("%.2f", subscription.Amount) + " subscription fee.")
 	return nil
 }
